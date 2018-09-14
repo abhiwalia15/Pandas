532b3e1f8866257feec7e94a6acccff5eb1d0c65
Mrinal <waliamrinal@gmail.com>
2018-09-14 16:24:13 +0530

    tesla and robinhood stock prices with date and time

---
+++ stocks_tesla.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import pandas_datareader.data as web_ui
+import datetime
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+style.use('ggplot')
+
+start = datetime.datetime(2010,1,1)
+end = datetime.datetime(2015,1,1)
+
+#define dataframe
+df = web_ui.DataReader('TSLA', 'robinhood', start, end)
+print(df.head())
+
+df['Adj Close'].plot()
+
+plt.show()
