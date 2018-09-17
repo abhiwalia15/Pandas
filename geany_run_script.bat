87cc39fdd9c0398934f3d29f30ed9bd56ef8247f
Mrinal <waliamrinal@gmail.com>
2018-09-17 18:38:17 +0530

    hindustan aero. stocks pandas analysis

---
+++ geany_run_script.bat
@@ -0,0 +1,6 @@
+python "pandas_3.py"
+
+pause
+del "%0"
+
+pause
--- pandas_3.py
+++ pandas_3.py
@@ -9,3 +9,13 @@ print(df.head())
 
 #save the result to a csv_file.
 df.to_csv('HINDUSTAN_Aeronautics Limited.csv')
+
+#save the file in html format
+df.to_html('HINDUSTAN_Aeronautics Limited.html')
+
+#if you dont want to print the header.
+df.to_csv('HINDUSTAN_Aeronautics Limited1.csv', header=False)
+
+#how to rename the columns
+df.rename(columns={'Close':'khtm'}, inplace=True)
+
